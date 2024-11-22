e6fe642ea71467b988a189a7c320939a651d0149
Piyush1532 <piyushadav503@gmail.com>
2024-11-22 14:12:02 +0530

    added some gsap animation

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const menuBar=document.querySelector(".fa-bars")
-const close=document.querySelector(".fa-close")
-const navbar=document.querySelector("#navbar")
+const menuBar = document.querySelector(".fa-bars");
+const close = document.querySelector(".fa-close");
+const navbar = document.querySelector("#navbar");
 
-menuBar.addEventListener("click",()=>{
-    navbar.classList.add("active")
-})
+menuBar.addEventListener("click", () => {
+  navbar.classList.add("active");
+});
 
+close.addEventListener("click", () => {
+  navbar.classList.remove("active");
+});
 
-close.addEventListener("click",()=>{
-    navbar.classList.remove("active")
-})
+// gsap animation start here
+
+const tL = gsap.timeline();
+
+tL.from(".home header", {
+  opacity: 0,
+  y: -100,
+  duration: 1.5,
+  ease: "power4.out",
+});
+
+tL.from(".home .main h1,.home .main p,.home .main .search", {
+  opacity: 0,
+  y: 100,
+  duration: 1.5,
+  ease: "power4.out",
+  stagger:.2
+});
